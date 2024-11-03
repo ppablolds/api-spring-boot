953b2d0e292ee31994568336ae92563d0f249dcb
Pablo Silva <pablosilvaaluno@outlook.com>
2024-11-03 17:13:17 -0300

    Create methods HTTP

--- src/main/java/com/dio/springboot/api/controller/UserController.java
+++ src/main/java/com/dio/springboot/api/controller/UserController.java
@@ -3,8 +3,11 @@ package com.dio.springboot.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dio.springboot.api.model.User;
@@ -22,5 +25,15 @@ public class UserController {
     @GetMapping("/users/{username}")
     public User findOne(@PathVariable("username") String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public void saveUser(@RequestBody User user) {
+        userRepository.save(user);
     }
 }
